c9c24c212c2f4b4a69ecf5bd9adbafac6c72b5ed
Jaksa Jokunen <testinumero123@gmail.com>
2022-08-26 11:39:55 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean weekday = true;
+        boolean vacation = true;
+
+        if(weekday == false && vacation == true)
+        {
+            System.out.println("James sleeps late");
+        }
+        else {
+            System.out.println("James doesn't sleep late");
+        }
+    }
+}
